acec7ba771cc643b9c5ea10fdebd9d3315624de6
Nishan <nishz1072@gmail.com>
2025-07-01 17:46:38 +0530

    Updated the JSON displayed on the page

--- app.vue
+++ app.vue
@@ -7,3 +7,14 @@
 <script setup lang="ts">
 
 </script>
+
+<style>
+.q-table{
+  border-radius: 12px !important;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.06); 
+}
+.q-input{
+  border-radius: 12px !important;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.06);
+}
+</style>
